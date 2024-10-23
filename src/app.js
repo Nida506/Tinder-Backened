cd1fb589e6a6bbb8996c79d4dcddbbcd19c3bf7d
NidaWaheed <109359002+Nidachaudary@users.noreply.github.com>
2024-10-23 13:37:56 +0500

    exploring get , post , delete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
-app.listen(3000, () => {
+app.listen(8888, () => {
   console.log("successfully connect");
 });
-app.use("/", (req, res) => {
-  console.log("Hello from the serever");
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Nida", lastName: "Waheed" });
+});
+app.post("/user", (req, res) => {
+  console.log("save data to database");
+  res.send("Data saved to the database successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfullly");
+});
+app.use("/test", (req, res) => {
+  res.send("Ok");
 });
