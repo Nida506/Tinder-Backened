14a111f18a4d938e9c53991f680e598500ec815a
NidaWaheed <109359002+Nidachaudary@users.noreply.github.com>
2025-02-06 15:29:32 +0500

    port on cors change

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -66,6 +66,7 @@ authRouter.post("/signIn", async (req, res) => {
     res.json({ message: "Logged in successfully", data: user });
   } catch (err) {
     res.status(400).send(err.message);
+    res.json({ message: "Not Logged in successfully", data: user });
   }
 });
 //logout api
